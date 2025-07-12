9d535d2b86f8bddd80adae3626d115ded8615b55
kweonminsung <kevin136583@gmail.com>
2025-07-12 16:08:39 +0900

    chore: 돌아가는 버튼 추가

--- frontend/src/components/common/sketchbook/Sketchbook.tsx
+++ frontend/src/components/common/sketchbook/Sketchbook.tsx
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import {
   SketchbookContainer,
   SketchbookBinding,
   SketchbookRing,
   SketchbookPage,
+  SketchbookPageWrapper,
 } from './Sketchbook.styles';
 
 export const Sketchbook = (props: { children: React.ReactNode }) => {
+  const [flipping, setFlipping] = useState(false);
+
+  const handleFlip = () => {
+    setFlipping(true);
+    setTimeout(() => {
+      setFlipping(false);
+    }, 700);
+  };
+
   const rings = Array.from({ length: 11 }, (_, i) => (
     <SketchbookRing key={i} />
   ));
@@ -13,7 +24,12 @@ export const Sketchbook = (props: { children: React.ReactNode }) => {
   return (
     <SketchbookContainer>
       <SketchbookBinding>{rings}</SketchbookBinding>
-      <SketchbookPage>{props.children}</SketchbookPage>
+      <SketchbookPageWrapper>
+        <SketchbookPage flipping={flipping}>{props.children}</SketchbookPage>
+      </SketchbookPageWrapper>
+      <div style={{ position: 'absolute', bottom: 20, right: 20 }}>
+        <button onClick={handleFlip}>위로 넘기기</button>
+      </div>
     </SketchbookContainer>
   );
 };
